201ef17b351c641df366bb7cc97304dbe812414a
sitammeur <sitammeur@gmail.com>
2024-09-22 11:23:03 +0000

    image store added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Editor from "@/components/Editor";
-import Image from "next/image";
+import { ImageStore } from "@/lib/image-store";
 
 export default function Home() {
   return (
-    <main>
-      <Editor />
-    </main>
+    <ImageStore.Provider initialValue={{ generating: false }}>
+      <main>
+        <Editor />
+      </main>
+    </ImageStore.Provider>
   );
 }
