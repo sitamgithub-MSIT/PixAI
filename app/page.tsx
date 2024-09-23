41ac8b8302b91d551f6420b81d6eecdce8d1823b
sitammeur <sitammeur@gmail.com>
2024-09-23 01:15:38 +0000

    layer store added

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,29 @@
 
 import Editor from "@/components/Editor";
 import { ImageStore } from "@/lib/image-store";
+import { LayerStore } from "@/lib/layer-store";
 
 export default function Home() {
   return (
-    <ImageStore.Provider initialValue={{ generating: false }}>
-      <main>
-        <Editor />
-      </main>
-    </ImageStore.Provider>
+    <LayerStore.Provider
+      initialValue={{
+        layers: [
+          {
+            id: crypto.randomUUID(),
+            url: "",
+            height: 0,
+            width: 0,
+            publicId: "",
+          },
+        ],
+        layerComparisonMode: false,
+      }}
+    >
+      <ImageStore.Provider initialValue={{ generating: false }}>
+        <main>
+          <Editor />
+        </main>
+      </ImageStore.Provider>
+    </LayerStore.Provider>
   );
 }
